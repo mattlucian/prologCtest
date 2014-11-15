6ad887529044410e9694ea54256bd5218578fc32
Matt <matt@Matts-MacBook-Pro.local>
2014-11-14 21:03:11 -0500

    Initial Commit

---
+++ prologCtest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  prologCtest
+//
+//  Created by Matt on 11/14/14.
+//  Copyright (c) 2014 Matt. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
